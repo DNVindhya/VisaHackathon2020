3b34d20941e9ba2caedfee272f9f7860ec58682c
Ankur Gokhale <ankurgokhale@Ankurs-MacBook-Pro.local>
2020-06-20 21:48:09 -0500

    Admin Panel and Database Schema Setup

--- admin-panel/admin.py
+++ admin-panel/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Consumer
+from .models import Merchant
+from .models import Offers
+from .models import Orders
+
+admin.site.register(Consumer)
+admin.site.register(Merchant)
+admin.site.register(Offers)
+admin.site.register(Orders)
