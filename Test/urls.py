e5c10b99c2fa22cafa4f94a81c0b7b53ac60c3a2
naghma <naghma111@gmail.com>
2020-06-24 16:41:05 -0500

    Offers app with view(edit,delete) offers

--- Test/urls.py
+++ Test/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -52,6 +52,7 @@ urlpatterns = [
     path('custsignup/', custsignup),
     path('merchlogin/', merchlogin),
     path('merchsignup/', merchsignup),
+    path('offers/',include('offers.urls')),
     
 ]
 
